78d0a8129c5536323243b4e78da55f9d1c687b74
Aaron Abraham <ronjohneden@gmail.com>
2021-11-05 19:54:50 +0530

    Added Product program

---
+++ ProductNum.java
@@ -0,0 +1,10 @@
+class Multiply
+{
+    public static void main (String args[])
+    {
+        int a=4;
+        int b =5;
+        int product=a*b;
+        System.out.println("The product is :"+product);
+    }
+}
